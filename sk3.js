3e0401fb72c21512eb13228d3dfcb68fbaf00d9f
Lionel <minjuwork2@gmail.com>
2026-08-02 13:14:05 +0700

    sk3

---
+++ sk3.js
@@ -0,0 +1,24 @@
+function filterParkirGanjilGenap(daftarPlat, tanggal) {
+    let diizinkan = [];
+    let index = 0;
+
+    let tanggalIsGenap = (tanggal % 2 === 0);
+
+    for (let i = 0; i < daftarPlat.length; i++) {
+        let plat = daftarPlat[i];
+        
+        let platIsGenap = (plat % 2 === 0);
+
+        if (platIsGenap === tanggalIsGenap) {
+            diizinkan[index] = plat; 
+            index++;                  
+        }
+    }
+    return diizinkan;
+}
+
+let platKendaraan = [1234, 9999, 8882, 1231];
+let tanggalHariIni = 24;
+
+let hasil = filterParkirGanjilGenap(platKendaraan, tanggalHariIni);
+console.log(hasil); 
